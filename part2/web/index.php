024778b346827adfc1995434b37d5e42245b40c0
php-nik <php-nik@mail.ru>
2017-02-18 14:13:36 +0300

    part 2

---
+++ part2/web/index.php
@@ -0,0 +1,18 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+Debug::enable();
+
+$kernel = new AppKernel('dev', true);
+
+$request = Request::createFromGlobals();
+
+$response = $kernel->handle($request);
+
+$response->send();
+
+$kernel->terminate($request, $response);
