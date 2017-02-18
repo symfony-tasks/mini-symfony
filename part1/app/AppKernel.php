35d11f84abc122037f214bca95c48199bc624b92
php-nik <php-nik@mail.ru>
2017-02-18 14:00:17 +0300

    part1

--- part1/app/AppKernel.php
+++ part1/app/AppKernel.php
@@ -7,9 +7,6 @@ use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\HttpKernel;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
-use Symfony\Component\Debug\Exception\FlattenException;
-use Symfony\Component\HttpKernel\EventListener\ExceptionListener;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class AppKernel extends Kernel
@@ -36,18 +33,6 @@ class AppKernel extends Kernel
 
         $container->setDefinition('event_dispatcher', new Definition(EventDispatcher::class));
         $container->setDefinition('controller_resolver', new Definition(ControllerResolver::class));
-    }
-
-    protected function initializeContainer()
-    {
-        parent::initializeContainer();
-
-        $dispatcher = $this->container->get('event_dispatcher');
-        
-        $dispatcher->addSubscriber(new ExceptionListener(function (FlattenException $exception) {
-            $msg = sprintf('Error %d: "%s"', $exception->getStatusCode(), $exception->getMessage());
-            return new Response($msg, $exception->getStatusCode());
-        }));
     }
 
 }
--- part1/web/index.php
+++ part1/web/index.php
@@ -1,14 +1,11 @@
 <?php
 
-use Symfony\Component\HttpKernel\HttpKernel;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Debug\Debug;
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__.'/../app/AppKernel.php';
+
+Debug::enable();
 
 $kernel = new AppKernel('dev', true);
 
