4c1d85b1966cd58861ebab524811a2991a5d0264
nighten <aa.golovanov@gmail.com>
2018-01-17 10:44:33 +0300

    first

---
+++ app.php
@@ -0,0 +1,13 @@
+<?php
+
+use Symfony\Component\Debug\Debug;
+use Symfony\Component\HttpFoundation\Request;
+
+$loader = require __DIR__ . '/vendor/autoload.php';
+Debug::enable();
+$request = Request::createFromGlobals();
+
+$kernel = new AppKernel('dev', true);
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
